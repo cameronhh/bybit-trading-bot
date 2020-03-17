7d7f15d0257c226f6fbec3ee187af4b68a784116
Cameron Harder-Hutton <chh@keniuren.lan>
2020-03-17 15:04:55 +1000

    rm comments - this file to be removed

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
+import time
 
-"""
-General Notes:
-    - project will be a 'forgetful' bot, meaning that it only stores enough data for it to calculate the indicators it uses
-    - forgetful also means that it will not need a database
-    - future intention is that it is not forgetful and stores everything
-    - there is an indicator library called 'ta' which can be used for most of the technical analysis
-
-Project structure:
-    - Bybit Client (downloaded)
-    - Kline data
-        - download from coinbase api - as data is better than the bybit API
-        - do technical analysis on the data
-        - define different states based on the technical analysis
-    - Logic for the client
-        - different actions based on different states
-        - stake size / risk management
-        - etc.
-
-"""
+from bot import TradingBot
 
 TIME_PERIOD = 5 # in mins
 TIME_PERIOD_SECS = TIME_PERIOD * 60
-
-from bot import TradingBot
-import time
-
 
 def main():
 
