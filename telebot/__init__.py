75120b9fda45918fd1c8d9193d9fd11376f9cf5d
Cameron Harder-Hutton <chh@keniuren.lan>
2020-04-15 20:25:45 +1000

    clean up old code

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -14,28 +14,15 @@ from telegram.ext import Filters
 
 from bot import TradingBot
 
-TRADE_SELECT = "trade_select"
-SHORT_TRADE = "short_trade"
-LONG_TRADE = "long_trade"
-OPEN_ORDERS = "open_orders"
-FREE_BALANCE = "free_balance"
+""" Playing around with a telegram bot to send updates to my phone when actions are executed
+    TODO: more on this
 
-CANCEL_ORD = "cancel_order"
-PROCESS_ORD_CANCEL = "process_ord_cancel"
+"""
 
-COIN_NAME = "coin_name"
-PERCENT_CHANGE = "percent_select"
-AMOUNT = "amount"
-PRICE = "price"
-PROCESS_TRADE = "process_trade"
+with open('secret/telegram_keys.json', 'r') as f:
+    user_info = json.load(f)
 
-CONFIRM = "confirm"
-CANCEL = "cancel"
-END_CONVERSATION = ConversationHandler.END
-
-
-CHAT_ID = 951313615
-
+print(user_info)
 
 class TelegramBot:
     def __init__(self, token: str, allowed_user_id, trading_bot: TradingBot):
@@ -59,24 +46,3 @@ class TelegramBot:
             update.message.reply_text(f"cute! hello {update.message.from_user.first_name}! i am autobot rollout")
 
         self.dispatcher.add_handler(CommandHandler('hello', hello))
-
-            
-
-
-
-
-# updater = Updater(token="1119907117:AAE7yP-2n3JCY0yBJVh1UxubwjIaf4RbsGQ", use_context=True)
-# dispatcher = updater.dispatcher
-
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-# logger = logging.getLogger(__name__)
-
-# def alarm(context):
-#     """Send the alarm message."""
-#     job = context.job
-#     context.bot.send_message(job.context, text='Beep!')
-
-# start_handler = CommandHandler('start', start)
-# dispatcher.add_handler(start_handler)
-
-# updater.start_polling()
